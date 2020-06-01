877eaeb52f9b4fd2401c938af07fdb2b62765466
Jesus Kauze <12-10273@usb.ve>
2020-06-01 17:09:37 -0400

    add forms and connect to twilio service for send sms to verified number

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
 const router = Router();
+const { sendMessage } = require('../twilio/send-sms')
 
 router.get('/', (req, res) => {
     res.render('index')
+})
+
+router.post('/send-sms', async (req, res) => {
+    const response = await sendMessage(req.body.message, req.body.phone)
+    console.log(response.sid)
+    res.send('Sended');
 })
 
 module.exports = router;
