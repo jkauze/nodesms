0ec75a02ba61ad605766250dfc75df7f0c44ec42
Jesus Kauze <12-10273@usb.ve>
2020-06-01 22:41:15 -0400

    implement socket.io to get incoming sms from phone USA. implement NotifyMe. implement timeago. small details

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 
 const app = require('./server');
+const http = require('http');
+
+// Server for socket.IO
+const server = http.createServer(app);
 
 require('./database');
+require('./sockets').connection(server);
 
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
 })
