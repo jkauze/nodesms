ee366d890bea2ebabf61e377817588ef030fe6e3
Jesus Kauze <12-10273@usb.ve>
2020-06-01 22:57:09 -0400

    final details?

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
     accountSid: process.env.TWILIO_ACCOUNT_SID,
     authToken: process.env.TWILIO_AUTH_TOKEN,
-    phone: '+584141635092'
+    phone: process.env.PHONENUMBER_VERIFIED
 }
